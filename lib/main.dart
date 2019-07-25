fbeb9c605e371d83b99b38f86c147c20138be838
Khalid F.Shuhail <dev.khalid@me.com>
2019-07-25 22:53:23 +0300

    [Localizations] Add localizations to project
    Add localizations functions and state to main entry of project TaleApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+//import 'package:flutter_cupertino_localizations/flutter_cupertino_localizations.dart';
 
 void main() => runApp(TaleApp());
 
@@ -8,14 +10,42 @@ class TaleApp extends StatefulWidget {
 }
 
 class _TaleAppState extends State<TaleApp> {
+  Locale currentLocale;
+
+  Future onLocaleChange(Locale newLocale) async {
+    setState(() {
+      currentLocale = newLocale;
+    });
+  }
+
+  Locale hundleLocaleCallback(
+      Locale deviceLocale, Iterable<Locale> appSupportedLocales) {
+    print('Device Locale: ' + deviceLocale.toString());
+    print('Tale App Supported Locales: ' + appSupportedLocales.toString());
+
+    currentLocale = appSupportedLocales.firstWhere(
+      (locale) => deviceLocale.languageCode == locale.languageCode,
+      orElse: () => Locale(
+        appSupportedLocales.first.languageCode,
+        deviceLocale.countryCode,
+      ),
+    );
+    return currentLocale;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Localisation Tale',
       theme: ThemeData(),
-      home: Center(
-        child: Text('**'),
-      ),
+      //locale: currentLocale,
+      supportedLocales: [Locale('en'), Locale('ar')],
+      localeResolutionCallback: hundleLocaleCallback,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: Center(child: Text(currentLocale.toString())),
     );
   }
 }
