de9b3d12a5857e9d278a11a38621f15f491e5f59
Khalid F.Shuhail <dev.khalid@me.com>
2019-07-26 06:01:22 +0300

    Add tab bar to with tab bar view

--- lib/tale_home_page.dart
+++ lib/tale_home_page.dart
@@ -7,7 +7,31 @@ class TaleHomePage extends StatefulWidget {
   _TaleHomePageState createState() => _TaleHomePageState();
 }
 
-class _TaleHomePageState extends State<TaleHomePage> {
+class _TaleHomePageState extends State<TaleHomePage>
+    with SingleTickerProviderStateMixin {
+  final List<Tab> _tabs = <Tab>[
+    Tab(text: 'WidgetsLocalizations'),
+    Tab(text: 'MaterialLocalizations'),
+    Tab(text: 'CupertinoLocalizations'),
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: _tabs.length,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   void switchLanguage(BuildContext context) {
     if (Localizations.localeOf(context).languageCode == 'en') {
       widget.onLocaleChange(Locale('ar'));
@@ -20,15 +44,26 @@ class _TaleHomePageState extends State<TaleHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('🌍(${Localizations.localeOf(context).toString()}):'),
         leading: IconButton(
           icon: Icon(Icons.translate),
           onPressed: () {
             switchLanguage(context);
           },
         ),
+        bottom: TabBar(
+          tabs: _tabs,
+          controller: _tabController,
+          isScrollable: true,
+        ),
       ),
-      body: Center(
-        child: Text(Localizations.localeOf(context).toLanguageTag()),
+      body: TabBarView(
+        controller: _tabController,
+        children: _tabs
+            .map(
+              (tab) => Center(child: tab),
+            )
+            .toList(),
       ),
     );
   }
